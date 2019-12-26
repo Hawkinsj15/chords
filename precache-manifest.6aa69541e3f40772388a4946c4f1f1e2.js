8a988dea1f5dace48e507bd5b3ccfc0c946f44c4
Hawkinsj15 <hawkinsj15@students.ecu.edu>
2019-12-26 02:41:02 -0500

    Updates

---
+++ precache-manifest.6aa69541e3f40772388a4946c4f1f1e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0692a6b55425021e33c94b9c5875cea",
+    "url": "/chords/index.html"
+  },
+  {
+    "revision": "588ab7d8a3e8ff092fcb",
+    "url": "/chords/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "27962194cd4f064147d2",
+    "url": "/chords/static/css/main.07fc18b9.chunk.css"
+  },
+  {
+    "revision": "588ab7d8a3e8ff092fcb",
+    "url": "/chords/static/js/2.a93a9853.chunk.js"
+  },
+  {
+    "revision": "27962194cd4f064147d2",
+    "url": "/chords/static/js/main.7667893b.chunk.js"
+  },
+  {
+    "revision": "fa320d82e86087a5a805",
+    "url": "/chords/static/js/runtime-main.927e7437.js"
+  }
+]);
